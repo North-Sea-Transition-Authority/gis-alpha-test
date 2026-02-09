9c254af9d1da89dedeeb3a3705052558c4e3991e
George O <115717712+GeorgeOG34@users.noreply.github.com>
2026-02-09 08:40:18 +0000

    GISA-53: convert point to E/W line (#31)

---
+++ src/main/java/uk/co/fivium/gisalphatest/feature/LineUtils.java
@@ -0,0 +1,23 @@
+package uk.co.fivium.gisalphatest.feature;
+
+import com.esri.core.geometry.Point;
+import com.esri.core.geometry.Polyline;
+
+public class LineUtils {
+
+  // 1 second in degrees (arc second) == 1° (degree) / 60'(minutes) / 60” (seconds)
+  public static final Double ONE_ARC_SECOND = 1 / 3600.0;
+
+  public static String pointToEastWestLineJson(double longitude, double latitude) {
+    var startPoint = new Point(longitude - ONE_ARC_SECOND, latitude);
+    var middlePoint = new Point(longitude, latitude);
+    var endPoint = new Point(longitude + ONE_ARC_SECOND, latitude);
+
+    var polyLine = new Polyline();
+    polyLine.startPath(startPoint);
+    polyLine.lineTo(middlePoint);
+    polyLine.lineTo(endPoint);
+
+    return polyLine.toString();
+  }
+}
